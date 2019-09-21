a7317cfbfb4d8b8a0acf09456408d1c724f15250
JUNGTAEK JOO <j3470@hotmail.com>
2019-09-21 17:54:40 +0900

    2019/09/21
    - UI 구현 중

---
+++ app/src/main/java/ga/project0511/graduationproject/GardeningGuide.java
@@ -0,0 +1,14 @@
+package ga.project0511.graduationproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GardeningGuide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gardening_guide);
+    }
+}
---
+++ app/src/main/java/ga/project0511/graduationproject/listing_plants.java
@@ -0,0 +1,14 @@
+package ga.project0511.graduationproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class listing_plants extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listing_plants);
+    }
+}
